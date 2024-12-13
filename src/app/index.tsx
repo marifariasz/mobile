e7762fd358c12f7fd8aa1b1d23a57b9c4705c777
marifariasz <engmarifarias@gmail.com>
2024-12-13 17:59:46 -0300

    Aula 1
    Finalização da aula 1 da NLW, feita pelo RocketSeat

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import {View, Text} from "react-native"
+
+import { Steps } from "@/components/steps"
+import { Welcome } from "@/components/welcome"
+import { Button } from "@/components/button"
+
+export default function Index(){
+
+    return(
+        <View 
+        style={{flex: 1, padding: 40, gap: 40}}>
+            <Welcome/>
+            <Steps />
+
+        </View>
+    )
+}
---
+++ src/components/button/index.tsx
@@ -0,0 +1,14 @@
+import { TouchableOpacity, TouchableOpacityProps, Text, TextProps } from "react-native";
+
+import {s} from "./styles"
+import { colors } from "@/styles/theme"
+
+function Button(){
+    <TouchableOpacity activeOpacity = {0.8}>
+        
+    </TouchableOpacity>
+}
+function Title({children}: TextProps){
+    return <Text></Text>
+}
+export {Button}
